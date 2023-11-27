1292c5620178241a0a502a5e6ceb1b5d3ee53798
Sanne Wintrén <sanne.wintren@gmail.com>
2023-11-27 19:28:21 +0100

    Add Button component

---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  min-width: 200px;
+  padding: 16px 40px 7px 40px;
+  text-align: center;
+
+  border-radius: 45px;
+  background: #34add3;
+  color: #fff;
+  font-size: 36px;
+  font-family: "BalooTamma2";
+
+  box-shadow: 0px 2px 7px 1px rgba(0, 0, 0, 0.25);
+
+  &:hover {
+    box-shadow: 0px 2px 7px 1px rgba(0, 0, 0, 0.25) inset;
+  }
+`;
