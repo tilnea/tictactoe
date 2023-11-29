2c902dd6b6668fbe6e3881ee826bfe4552ad12b9
Sanne Wintrén <sanne.wintren@gmail.com>
2023-11-29 08:01:58 +0100

    Add more specific child-selectors in Tie component

--- src/components/tie.tsx
+++ src/components/tie.tsx
@@ -23,47 +23,47 @@ const Wrapper = styled.div`
   font-size: 64px;
   color: #000;
 
-  > span {
+  > ${Cell} {
     opacity: 0;
     animation-direction: alternate;
   }
 
-  :nth-child(1) {
+  ${Cell}:nth-child(1) {
     font-family: Barriecito;
     animation: ${opacity} 2.2s ease-in-out 1;
   }
 
-  :nth-child(4) {
+  ${Cell}:nth-child(4) {
     font-family: Courgette;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 0.2s;
   }
 
-  :nth-child(7) {
+  ${Cell}:nth-child(7) {
     font-family: DancingScript;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 0.4s;
   }
 
-  :nth-child(2) {
+  ${Cell}:nth-child(2) {
     font-family: DancingScript;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 0.6s;
   }
 
-  :nth-child(3) {
+  ${Cell}:nth-child(3) {
     font-family: DancingScript;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 0.8s;
   }
 
-  :nth-child(5) {
+  ${Cell}:nth-child(5) {
     font-family: Barriecito;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 1s;
   }
 
-  :nth-child(9) {
+  ${Cell}:nth-child(9) {
     font-family: Barriecito;
     animation: ${opacity} 0.9s ease-in-out 1;
     animation-delay: 1.2s;
